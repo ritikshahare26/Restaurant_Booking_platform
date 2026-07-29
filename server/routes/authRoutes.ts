38ebd3fb1ff1f2c66118c0681477609e547b0ec6
Ritik Shahare <ritikshahare26@gmail.com>
2026-07-29 23:03:51 +0530

    create the middleware folder

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,12 +1,11 @@
-
-import { Router}from "express";
-import {  loginUser, registerUser } from "../controllers/authcontroller.js";
+import { Router } from "express";
+import { loginUser, registerUser, getMe } from "../controllers/authcontroller.js";
 import { protect } from "../middlewares/auth.js";
   
 const authRouter = Router()
 
-authRouter.post ("/register", registerUser)
-authRouter.post ("login", loginUser)
-authRouter.get ("/me",protect, getMe)
+authRouter.post("/register", registerUser)
+authRouter.post("/login", loginUser)
+authRouter.get("/me", protect, getMe)
 
 export default authRouter;
